26a807975a5ecb06c3537f097eb265ce92841e1f
alankar-ttn <alankar.saxena@tothenew.com>
2022-08-21 11:05:50 +0530

    time slot api changes

--- users/views.py
+++ users/views.py
@@ -404,39 +404,37 @@ class OperatorWiseTimeSlotsApiView(generics.RetrieveAPIView):
         date = request.GET.get("date")
         attendance = Attendance.objects.get(user=operator, date=date)
         time_slots = []
-        time_slots.append(
-            {
-                "slot":"10:00 AM - 11:00 AM",
-                "status":attendance.slot_10_to_11
-            },
-            {
-                "slot":"11:00 AM - 12:00 PM",
-                "status":attendance.slot_11_to_12
-            },
-            {
-                "slot":"12:00 PM - 1:00 PM",
-                "status":attendance.slot_12_to_1
-            },
-            {
-                "slot":"1:00 PM - 2:00 PM",
-                "status":attendance.slot_1_to_2
-            },
-            {
-                "slot":"2:00 PM - 3:00 PM",
-                "status":attendance.slot_2_to_3
-            },
-            {
-                "slot":"3:00 PM - 4:00 PM",
-                "status":attendance.slot_3_to_4
-            },
-            {
-                "slot":"4:00 PM - 5:00 PM",
-                "status":attendance.slot_4_to_5
-            },
-            {
-                "slot":"5:00 PM - 6:00 PM",
-                "status":attendance.slot_5_to_6
-            }
-        )
+        time_slots.append({
+            "slot":"10:00 AM - 11:00 AM",
+            "status":attendance.slot_10_to_11
+        })
+        time_slots.append({
+            "slot":"11:00 AM - 12:00 PM",
+            "status":attendance.slot_11_to_12
+        })
+        time_slots.append({
+            "slot":"12:00 PM - 1:00 PM",
+            "status":attendance.slot_12_to_1
+        })
+        time_slots.append({
+            "slot":"1:00 PM - 2:00 PM",
+            "status":attendance.slot_1_to_2
+        })
+        time_slots.append({
+            "slot":"2:00 PM - 3:00 PM",
+            "status":attendance.slot_2_to_3
+        })
+        time_slots.append({
+            "slot":"3:00 PM - 4:00 PM",
+            "status":attendance.slot_3_to_4
+        })
+        time_slots.append({
+            "slot":"4:00 PM - 5:00 PM",
+            "status":attendance.slot_4_to_5
+        })
+        time_slots.append({
+            "slot":"5:00 PM - 6:00 PM",
+            "status":attendance.slot_5_to_6
+        })
         return success_response(time_slots)
 
