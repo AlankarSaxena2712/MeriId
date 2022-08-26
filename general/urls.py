d6bdeb827404737ff5d33392a13ec36c2638dc5f
alankar-ttn <alankar.saxena@tothenew.com>
2022-08-26 14:56:52 +0530

    csrf exempt

--- general/urls.py
+++ general/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from django.views.decorators.csrf import csrf_exempt
+
 from general.views import FeedbackView, GuidelinesView, NoticeView, AadharAddressPrePopulateAPI, sendXml
 
 urlpatterns = [
@@ -7,6 +9,6 @@ urlpatterns = [
     path("feedback", FeedbackView.as_view(), name="feedback"),
     path("notice", NoticeView.as_view(), name="notice"),
     path("populate-address/<str:aadhar>", AadharAddressPrePopulateAPI.as_view(), name="populate-address"),
-    path("get_xml", sendXml, name="sendXMl")
+    path("get_xml", csrf_exempt(sendXml), name="sendXMl")
     # path("phone-number-otp", otpTelegram(), name="telegram"),
 ]
